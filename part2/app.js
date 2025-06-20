a791efe4515c5a00ce085f895b744e0240a16a3a
hongdoudiaoyushao <hchengxi2005au@163.com>
2025-06-21 07:07:42 +0800

    Q14: Add a logout button on both dashboards that ends the session,erases the cookie data and returns to the login form.

--- part2/app.js
+++ part2/app.js
@@ -3,10 +3,18 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
+const session = require('express-session');
 
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(session({
+    secret: '123456',
+    resave: false,
+    saveUninitialized: false,
+    //https set to true
+    cookie: { secure: false }
+}));
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
