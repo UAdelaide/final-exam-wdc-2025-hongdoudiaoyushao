cee36953f45b61140b9e3b765600c89fb8da4a75
hongdoudiaoyushao <hchengxi2005au@163.com>
2025-06-21 10:13:44 +0800

    Revert "1"
    This reverts commit 86e31db64b09b20d793ee86a573af1ad020c1270.

---
+++ part2/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+const session = require('express-session');
+
+// Middleware
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(session({
+    secret: '123456',
+    resave: false,
+    saveUninitialized: false,
+    //https set to true
+    cookie: {secure: false}
+}));
+
+// Routes
+const walkRoutes = require('./routes/walkRoutes');
+const userRoutes = require('./routes/userRoutes');
+const dogRoutes = require('./routes/dogRoutes');
+
+app.use('/api/walks', walkRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api', dogRoutes);
+
+// Export the app instead of listening here
+module.exports = app;
